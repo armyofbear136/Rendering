de40b912e6978441a5b3b69b869eef6ff44a9fcf
Richard Polidoro <armyofbear136@gmail.com>
2017-11-10 13:44:29 -0600

    threejs lines model

--- build/app.bundle.js
+++ build/app.bundle.js
@@ -3160,6 +3160,22 @@ var THREE = _interopRequireWildcard(_three);
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
+// const express = require('express');
+// const app = express();
+// app.use('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '..', './index.html'));
+// });
+
+// const PORT = 8000;
+
+// const startListening = () => {
+//   // start listening (and create a 'server' object representing our server)
+//   const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+//   // set up our socket control center
+//   // const io = socketio(server);
+//   // require('./socket')(io);
+// };
 // const Detector = require('../tools/Detector');
 
 // if (Detector.webgl) {
@@ -3170,29 +3186,55 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 //   document.getElementById('container').appendChild(warning);
 // }
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// SPINNING CUBE 
+
+// const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth * .9, window.innerHeight * .9);
+// document.body.appendChild(renderer.domElement);
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+// camera.position.z = 5;
+
+// function animate() {
+//   requestAnimationFrame(animate);
+
+//   cube.rotation.x += 0.1;
+//   cube.rotation.y += 0.1;
+
+//   renderer.render(scene, camera);
+// }
+// animate();
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth * .9, window.innerHeight * .9);
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+camera.position.set(0, 0, 100);
+camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-camera.position.z = 5;
+var scene = new THREE.Scene();
 
-function animate() {
-  requestAnimationFrame(animate);
+//create a blue LineBasicMaterial
+var material = new THREE.LineBasicMaterial({ color: 0x0000ff });
 
-  cube.rotation.x += 0.1;
-  cube.rotation.y += 0.1;
+var geometry = new THREE.Geometry();
+geometry.vertices.push(new THREE.Vector3(-10, 0, 0));
+geometry.vertices.push(new THREE.Vector3(0, 10, 0));
+geometry.vertices.push(new THREE.Vector3(10, 0, 0));
 
-  renderer.render(scene, camera);
-}
-animate();
+var line = new THREE.Line(geometry, material);
+
+scene.add(line);
+renderer.render(scene, camera);
 
 /***/ })
 /******/ ]);
--- public/app.js
+++ public/app.js
@@ -1,4 +1,20 @@
 import * as THREE from 'three';
+// const express = require('express');
+// const app = express();
+// app.use('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '..', './index.html'));
+// });
+
+// const PORT = 8000;
+
+// const startListening = () => {
+//   // start listening (and create a 'server' object representing our server)
+//   const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+//   // set up our socket control center
+//   // const io = socketio(server);
+//   // require('./socket')(io);
+// };
 // const Detector = require('../tools/Detector');
 
 // if (Detector.webgl) {
@@ -9,26 +25,52 @@ import * as THREE from 'three';
 //   document.getElementById('container').appendChild(warning);
 // }
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth * .9, window.innerHeight * .9);
+// SPINNING CUBE 
+
+// const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth * .9, window.innerHeight * .9);
+// document.body.appendChild(renderer.domElement);
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+// camera.position.z = 5;
+
+// function animate() {
+//   requestAnimationFrame(animate);
+
+//   cube.rotation.x += 0.1;
+//   cube.rotation.y += 0.1;
+
+//   renderer.render(scene, camera);
+// }
+// animate();
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+camera.position.set(0, 0, 100);
+camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-camera.position.z = 5;
+var scene = new THREE.Scene();
 
-function animate() {
-  requestAnimationFrame(animate);
+//create a blue LineBasicMaterial
+var material = new THREE.LineBasicMaterial({ color: 0x0000ff });
 
-  cube.rotation.x += 0.1;
-  cube.rotation.y += 0.1;
+var geometry = new THREE.Geometry();
+geometry.vertices.push(new THREE.Vector3(-10, 0, 0));
+geometry.vertices.push(new THREE.Vector3(0, 10, 0));
+geometry.vertices.push(new THREE.Vector3(10, 0, 0));
 
-  renderer.render(scene, camera);
-}
-animate();
+var line = new THREE.Line(geometry, material);
+
+scene.add(line);
+renderer.render(scene, camera);
